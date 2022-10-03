8c5ee804e3270fdceef95e490b70203e8dd78e68
Yaren Yakup <a.yarenyakup@gmail.com>
2022-10-03 19:38:08 +0300

    Add project files.

---
+++ algorithm exercises/Program.cs
@@ -0,0 +1,32 @@
+﻿int a = 0;
+int b = 0;
+int result = 0;
+
+int[] arr = { 73, 67, 38, 33, 85 };
+
+
+for (int i = 0; i < arr.Length; i++)
+{
+    a = arr[i] % 5;
+    if (3 >= a)
+    {
+        b = 5 - a;
+        if (b < 3)
+        {
+            result = b + arr[i];
+            if (result < 40)
+            {
+                Console.WriteLine(arr[i]);
+
+                continue;
+            }
+            Console.WriteLine(result);
+        }
+        else
+        {
+            Console.WriteLine(arr[i]);
+        }
+
+    }
+
+}
