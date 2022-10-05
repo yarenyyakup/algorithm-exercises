222b3343434835a373552049beae2732ecfe3bad
Yaren Yakup <a.yarenyakup@gmail.com>
2022-10-05 14:32:44 +0300

    Add project files.

---
+++ conversion time/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace AccessArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int hour;
+            int minute;
+            int second;
+            String dayPart;
+            int time;
+
+            Console.Write("enter time\n");
+            hour = Convert.ToInt32(Console.ReadLine());
+            minute = Convert.ToInt32(Console.ReadLine());
+            second = Convert.ToInt32(Console.ReadLine());
+            dayPart = Console.ReadLine();
+
+            Console.WriteLine(hour + "." + minute + "." + second + dayPart);
+
+            if (dayPart == "PM")
+            {
+                hour = hour + 12;
+                Console.WriteLine(hour + "." + minute + "." + second);
+
+
+            }
+            else
+            {
+                if (hour < 10)
+                {
+                    Console.WriteLine("0" + hour + "." + minute + "." + second);
+                }
+                else
+                {
+                    Console.WriteLine(hour + "." + minute + "." + second);
+                }
+
+
+            }
+        }
+    }
+}
+
+
