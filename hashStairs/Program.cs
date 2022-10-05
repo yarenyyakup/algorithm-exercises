e76d28a6fd4b2b7a6db27479fbd0321a826c625b
Yaren Yakup <a.yarenyakup@gmail.com>
2022-10-05 16:40:17 +0300

    Add project files.

---
+++ hashStairs/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Program
+{
+    public static void hashStairs(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+
+            for (int j = 1; j <= n; j++)
+            {
+                if (j <= n - i)
+                {
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.Write("#");
+                }
+            }
+            Console.WriteLine();
+        }
+        
+    }
+
+    static void Main(string[] args)
+    {
+        hashStairs(6);
+    }
+}
